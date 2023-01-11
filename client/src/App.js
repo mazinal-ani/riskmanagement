8f82301b2c4dbe989c67503806054664e39552e3
Mazin Al-Ani <mazinal-ani@v1040-wn-rt-a-57-80.campus-dynamic.uwaterloo.ca>
2023-01-11 17:15:13 -0500

    Fixed percent change error

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function StockWeightForm() {
   const [stockData, setStockData] = useState([]);
   const [weightData, setWeightData] = useState([]);
+  const [confidenceLevel, setConfidenceLevel] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -10,7 +11,8 @@ function StockWeightForm() {
 
     const data = {
       stockData,
-      weightData
+      weightData,
+      confidenceLevel
     };
 
 
@@ -45,6 +47,15 @@ function StockWeightForm() {
           type="text"
           value={weightData}
           onChange={event => setWeightData(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Confidence Level:
+        <input
+          type="text"
+          value={confidenceLevel}
+          onChange={event => setConfidenceLevel(event.target.value)}
         />
       </label>
       <br />
--- server/calculations.py
+++ server/calculations.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+import math
 from scipy.stats import norm
 import yfinance as yf
 #import pandas as pd
@@ -13,35 +14,37 @@ CORS(app)
 def valueatrisk():
         
     data = request.get_json()
-    print(data)
+    #print(data)
     stocks = data['stockData'].split()
+    confidence_level = eval(data['confidenceLevel']) / 100
     weights = data['weightData'].split()
     weights = [eval(stock) for stock in weights]
     size = len(stocks)
+    #print (weights)
+    #print(size)
+    #print(stocks)
     
 
     def daily_return(historical_df):
 
-        daily_returns = [None]
+        daily_returns = []
 
-        for i in range(historical_df.shape[0] - 1):
+        for i in range(1, len(historical_df)):
 
-            day_close = historical_df.loc[historical_df.index[i], 'Close']
-            next_close = historical_df.loc[historical_df.index[i+1], 'Close']
-            percent_change = ((next_close / day_close) - 1) * 100
+            percent_change = (historical_df.iloc[i, 0] / historical_df.iloc[i - 1, 0]) - 1
             daily_returns.append(percent_change)
 
-            return daily_returns
+        return daily_returns
 
 
     def asset_variance(historical_df, mean):
 
         variance = 0
 
-        for i in range(historical_df.shape[0]):
-            variance += (mean - historical_df.loc[historical_df.index[i], 'Percent Change']) ** 2
+        for i in historical_df["Percent Change"]:
+            variance += (mean - i) ** 2
         
-        variance /= historical_df.shape[0] - 1
+        variance /= (len(historical_df) - 1)
 
         return variance
 
@@ -71,21 +74,33 @@ def valueatrisk():
             ticker = yf.Ticker(stock_symbol)
             ticker_historical = ticker.history(period='1y', interval='1d')
 
+            del ticker_historical['Open']
+            del ticker_historical['High']
+            del ticker_historical['Low']
+            del ticker_historical['Volume']
+            del ticker_historical['Dividends']
+            del ticker_historical['Stock Splits']
+
             daily_change_arr = daily_return(ticker_historical)
             total_daily_returns.append(daily_change_arr)
+            print(daily_change_arr)
 
+
+            ticker_historical = ticker_historical.tail(-1)
             ticker_historical['Percent Change'] = daily_change_arr
-            ticker_historical.drop(index=0, inplace=True)
+            
+            
 
-            start = ticker_historical.loc[ticker_historical.index[0], 'Close']
-            end = ticker_historical.loc[ticker_historical.index[-1], 'Close']
+            start = ticker_historical.iloc[0, 0]
+            end = ticker_historical.iloc[-1, 0]
 
-            mean_return = (((end / start) - 1) * 100) / ticker_historical.shape[0]
+            mean_return = math.e ** (math.log(end/start) / len(ticker_historical))
 
             variance = asset_variance(ticker_historical, mean_return)
 
             mean_returns.append(mean_return)
             variances.append(variance)
+            print("Hello")
 
         covariances = find_covariances(total_daily_returns)
 
@@ -100,15 +115,15 @@ def valueatrisk():
         portfolio_std = portfolio_variance ** 0.5
 
 
-
         z_score = norm.ppf(confidence_level)
 
         value_at_risk = portfolio_std * z_score
 
+        
         return (value_at_risk)
 
-    return jsonify({'Value at Risk': calculate_var(size, 0.95, stocks, weights)})
-
+    return jsonify({'Value at Risk': calculate_var(size, confidence_level, stocks, weights)})
+    #return jsonify("Hello")
 
 
 
