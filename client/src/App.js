4383cf62ee06f485ab754527c3b934663088364a
Mazin Al-Ani <mazinal-ani@v1040-wn-rt-a-57-80.campus-dynamic.uwaterloo.ca>
2023-01-11 19:28:11 -0500

    Fixed frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function StockWeightForm() {
   const [confidenceLevel, setConfidenceLevel] = useState([]);
   const [initialInvestment, setInitialInvestment] = useState([]);
   const [timePeriod, setTimePeriod] = useState([]);
+  const [responseData, setResponseData] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -30,59 +31,73 @@ function StockWeightForm() {
     })
       .then(response => response.json())
       .then(result => {
-        console.log(result);
+        setResponseData(result);
       });
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Stock Data:
-        <input
-          type="text"
-          value={stockData}
-          onChange={event => setStockData(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Weight Data:
-        <input
-          type="text"
-          value={weightData}
-          onChange={event => setWeightData(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Confidence Level:
-        <input
-          type="text"
-          value={confidenceLevel}
-          onChange={event => setConfidenceLevel(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Initial Investment (Dollars):
-        <input
-          type="text"
-          value={initialInvestment}
-          onChange={event => setInitialInvestment(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Time Period (Days):
-        <input
-          type="text"
-          value={timePeriod}
-          onChange={event => setTimePeriod(event.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+        <form onSubmit={handleSubmit}>
+        <label>
+          Stock Data:
+          <input
+            type="text"
+            value={stockData}
+            onChange={event => setStockData(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Weight Data:
+          <input
+            type="text"
+            value={weightData}
+            onChange={event => setWeightData(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Confidence Level:
+          <input
+            type="text"
+            value={confidenceLevel}
+            onChange={event => setConfidenceLevel(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Initial Investment (Dollars):
+          <input
+            type="text"
+            value={initialInvestment}
+            onChange={event => setInitialInvestment(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Time Period (Days):
+          <input
+            type="text"
+            value={timePeriod}
+            onChange={event => setTimePeriod(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {responseData === null ? (
+          <h1>
+            Hello
+          </h1>
+        ) : (
+          <div>
+            <h1>Here is your encrypted message:</h1>
+            <h1>{responseData}</h1>
+          </div>
+        )}
+      </div>
+    </div>  
   );
 }
 
